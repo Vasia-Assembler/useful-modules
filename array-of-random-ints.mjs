685ae4b792297eb8da88e56e89328c0c972f7741
Vasia-Assembler <112320206+Vasia-Assembler@users.noreply.github.com>
2022-10-03 13:39:25 +0300

    rename to .mjs

--- array-of-random-ints.mjs
+++ array-of-random-ints.mjs
@@ -1,4 +1,4 @@
-import randomInt from "./random-int"
+import randomInt from "./random-int.mjs"
 
 export default function randomInts(length, min, max, unique) {
   if (unique) {
@@ -6,6 +6,7 @@ export default function randomInts(length, min, max, unique) {
     do {
       nums.add(randomInt({ min, max }))
     } while (nums.size !== length)
+    return Array.from(nums)
   } else {
     return Array.from({length}, () => randomInt({ min, max }))
   }
--- random-int.mjs
+++ random-int.mjs
@@ -1,7 +1,7 @@
 export default function randomInt(args) {
   const min = args.min ?? 0
   const max = args.max ?? 1
-  const start_inclusive = args.inclusive.start
-  const end_inclusive = args.include.end
+  const start_inclusive = args.inclusive?.start
+  const end_inclusive = args.inclusive?.end
   return Math.floor(Math.random() * (max - min + 1) + min)
 }
