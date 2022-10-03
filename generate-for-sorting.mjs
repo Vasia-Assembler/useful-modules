7d14e8f1fcef6c50f2b17830dcc8585c5058115e
Vasia-Assembler <112320206+Vasia-Assembler@users.noreply.github.com>
2022-10-03 14:45:10 +0300

    update generate-for-sorting module

--- generate-for-sorting.mjs
+++ generate-for-sorting.mjs
@@ -2,5 +2,5 @@ import randomInts from "./array-of-random-ints.mjs";
 
 export default function generateForSorting(len, min, max, unique, sep) {
   const arr = randomInts(len, min, max, unique)
-  return `${arr.length}` + '\n' + arr.join(sep) + '\n'
+  return [arr, `${arr.length}` + '\n' + arr.join(sep) + '\n']
 }
